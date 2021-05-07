d59526a28e4831e40634f8fd43bd43ff8db6583e
Apoorv Verma <apoorv.verma999@gmail.com>
2021-05-07 18:31:04 +0530

    Almost done!

--- components/features.js
+++ components/features.js
@@ -2,7 +2,7 @@ const Features = () => {
     return  (
         <div className="">
             <div className="flex flex-wrap justify-center items-center lg:mt-80 my-10">
-                <img className="pt-10 max-w-sm md:max-w-md" src="/feat1.png"/>
+                <img className="my-10 max-w-sm md:max-w-lg" src="/feat1.png"/>
                 <div className="max-w-md pl-6 pt-10">
                     <p className="text-gray-500 font-bold font-sans text-lg">💼 we ensure the fast and easy</p>
                     <p className="font-extrabold font-sans text-4xl pt-5">Manage finance with security and joy.</p>
@@ -13,7 +13,7 @@ const Features = () => {
                 </div>
             </div>
             <div className="flex flex-wrap-reverse justify-center items-center my-10">
-                <div className="max-w-md pl-6 pt-10">
+                <div className="max-w-md pl-6">
                     <p className="text-gray-500 font-bold font-sans text-lg">💼 we ensure the fast and easy</p>
                     <p className="font-extrabold font-sans text-4xl pt-5">Multiple useful options in one platform.</p>
                     <p className="w-3/5 py-5 text-lg">We help banks to extend their offers and transform their cost structures.</p>
@@ -21,10 +21,10 @@ const Features = () => {
                     Unlock All Features ➞
                 </button>
                 </div>
-                <img className="pt-10 max-w-sm md:max-w-md" src="/feat2.png"/>
+                <img className="my-10 max-w-sm md:max-w-lg" src="/feat2.png"/>
             </div>
             <div className="flex flex-wrap justify-center items-center my-10">
-                <img className=" rounded-xl mt-10 max-w-sm md:max-w-lg  lg:max-w-xl md:mr-10 shadow-xl" src="/feat3.jpeg"/>
+                <img className=" rounded-xl my-10 max-w-sm md:max-w-lg shadow-xl" src="/feat3.jpeg"/>
                 <div className="max-w-md pl-6 pt-10">
                     <p className="text-gray-500 font-bold font-sans text-lg">💼 we ensure the fast and easy</p>
                     <p className="font-extrabold font-sans text-4xl pt-5">Easier way to transfer money from one user to another.</p>
@@ -43,7 +43,7 @@ const Features = () => {
                     Unlock All Features ➞
                 </button>
                 </div>
-                <img className="rounded-xl mt-10 max-w-sm md:max-w-lg lg:max-w-xl md:ml-10 shadow-xl" src="/feat4.jpeg"/>
+                <img className="rounded-xl my-10 max-w-sm md:max-w-lg shadow-xl" src="/feat4.jpeg"/>
             </div>
         </div>
     )
--- pages/index.js
+++ pages/index.js
@@ -18,25 +18,3 @@ const Home = () => {
 }
 
 export default Home;
-
-/*Navbar **done
-
-MainIntro **done
-
-Buttons **done
-
-Basic tile  
-
-Hover tiles
-
-Features I
-
-Features II
-
-Features III
-
-Features IV
-
-Newsletter  **done
-
-*/
